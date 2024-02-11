7097b7b49c4b515741106eb0d2d05b69d0c9402e
lpuliuname <qz77269@yeah.net>
2024-02-12 05:24:24 +0800

    Done

--- advanced/2DPlatformer/Game.lua
+++ advanced/2DPlatformer/Game.lua
@@ -3,7 +3,32 @@ local Game = fg.Object:extend('Game')
 Player = require 'Player'
 
 function Game:new()
+    fg.world.box2d_world:setGravity(0, 20*32)
+    fg.screen_scale = 2
+    fg.setScreenSize(960, 720)
     fg.world:createEntity('Player', fg.screen_width/2, fg.screen_height/2, {w = 16, h = 28})
+
+    fg.world:addLayer('BG1', {parallax_scale = 0.8})
+    fg.world:addToLayer('BG1', fg.Background(380, 260, love.graphics.newImage('bg-back.png')))
+    fg.world:addLayer('BG2', {parallax_scale = 0.9})
+    fg.world:addToLayer('BG2', fg.Background(440, 270, love.graphics.newImage('bg-mid.png')))
+    fg.world:setLayerOrder({'BG1', 'BG2', 'Default'})
+
+    tilemap = fg.Tilemap(fg.screen_width/2, fg.screen_height/2, 32, 32, love.graphics.newImage('tileset-normal.png'), {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1},
+        {1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1},
+        {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+        {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+        {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    })
+    tilemap:setAutoTileRules({6, 14, 12, 2, 10, 8, 7, 15, 13, 3, 11, 9})
+    tilemap:autoTile()
+    fg.world:generateCollisionSolids(tilemap)
+    fg.world:addToLayer('Default', tilemap)
 end
 
 function Game:update(dt)
--- advanced/2DPlatformer/Player.lua
+++ advanced/2DPlatformer/Player.lua
@@ -1,20 +1,35 @@
 local Player = fg.Class('Player', 'Entity')
 Player:implement(fg.PhysicsBody)
 
+Player.enter = {'Solid'}
+
 function Player:new(area, x, y, settings)
     Player.super.new(self, area, x, y, settings)
     self:physicsBodyNew(area, x, y, settings)
 
+    self.fixture:setFriction(0)
+
+    fg.world.camera:follow(self, {lerp = 1, follow_style = 'platformer'})
+
     self.direction = 'right'
     self.animation_state = 'idle'
+    self.jumping = false
     self.idle = fg.Animation(love.graphics.newImage('idle.png'), 32, 32, 0)
-    self.run = fg.Animation(love.graphics.newImage('run.png'), 32, 32, 0.1)
+    self.run = fg.Animation(love.graphics.newImage('run.png'), 32, 32, 0.18)
+    self.jump = fg.Animation(love.graphics.newImage('jump.png'), 32, 32, 0)
+    self.fall = fg.Animation(love.graphics.newImage('fall.png'), 32, 32, 0)
+
+    self.max_jumps = 1
+    self.jumps_left = self.max_jumps
+    self.jump_press_time = 0
 
     fg.input:bind('a', 'move_left')
     fg.input:bind('d', 'move_right')
     fg.input:bind('dpleft', 'move_left')
     fg.input:bind('dpright', 'move_right')
     fg.input:bind('leftx', 'move_horizontal')
+    fg.input:bind(' ', 'jump')
+    fg.input:bind('fdown', 'jump')
 end
 
 function Player:update(dt)
@@ -34,7 +49,27 @@ function Player:update(dt)
         local vx, vy = self.body:getLinearVelocity()
         self.body:setLinearVelocity(48*dt*vx, vy)
     end
+    if fg.input:pressed('jump') then
+        if self.jumps_left > 0 then
+            self.jumping = true
+            local vx, vy = self.body:getLinearVelocity()
+            self.body:setLinearVelocity(vx, -250)
+            self.jumps_left = self.jumps_left - 1
+            self.jump_press_time = love.timer.getTime()
+        end
+    end
+    if fg.input:released('jump') then
+        local stopJump = function()
+            self.jump_press_time = 0
+            local vx, vy = self.body:getLinearVelocity()
+            if vy < 0 then self.body:setLinearVelocity(vx, 0) end
+        end
+        local dt = love.timer.getTime() - self.jump_press_time
+        if dt >= 0.125 then stopJump()
+        else fg.timer:after(0.125 - dt, function() stopJump() end) end
+    end
 
+    --[[
     local x = fg.input:down('move_horizontal')
     if x then
         if x < 0 then 
@@ -50,10 +85,14 @@ function Player:update(dt)
         local vx, vy = self.body:getLinearVelocity()
         self.body:setLinearVelocity(48*dt*vx, vy)
     end
+    ]]--
 
     local vx, vy = self.body:getLinearVelocity()
     if math.abs(vx) < 25 then self.animation_state = 'idle'
     else self.animation_state = 'run' end
+    if self.jumping then self.animation_state = 'jump' end
+    if vy > 5 then self.animation_state = 'fall' end
+    self[self.animation_state]:update(dt)
     self[self.animation_state]:update(dt)
 end
 
@@ -67,6 +106,17 @@ function Player:draw()
         self[self.animation_state]:draw(self.x, self.y, 0, -1, 1, 
                                         self[self.animation_state].frame_width/2, 
                                         self[self.animation_state].frame_height/2 + 2)
+    end
+end
+
+function Player:onCollisionEnter(other, contact)
+    if other.tag == 'Solid' then
+        local solid_top = other.object.y - other.object.h/2
+        local player_bottom = self.y + self.h/2 - 4
+        if solid_top > player_bottom then
+            self.jumping = false
+            self.jumps_left = self.max_jumps
+        end
     end
 end
 
